8c959b8620c55eac2258376cd4f5f8f70167b15c
Abhiram <abhiram090601@gmail.com>
2026-03-02 17:04:54 +0530

    python remove basedpyright and add ruff and ty

--- lua/lsp/gopls.lua
+++ lua/lsp/gopls.lua
@@ -2,7 +2,7 @@
 
 return {
 	cmd = { "gopls" },
-	filetypes = { "go", "gomod", "gowork", "gotmpl" },
+	filetypes = { "go", "gomod" },
 	root_markers = { "go.work", "go.mod", ".git" },
 	settings = {
 		gopls = {
